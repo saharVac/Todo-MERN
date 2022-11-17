309fa87fdae5a9fb4f3584f5becf38b708ca8a64
saharv-av <vcsahar@gmail.com>
2022-11-17 18:07:48 -0500

    fixing routes to heroku hosted backend

--- src/DataHandling.js
+++ src/DataHandling.js
@@ -2,22 +2,22 @@ import Axios from 'axios'
 
 
 export const addToList = (task, days) => {
-    return Axios.post("http://localhost:3001/insert", { todoName: task, daysToDo: days })
+    return Axios.post("https://crudmern-todo.herokuapp.com/insert", { todoName: task, daysToDo: days })
 }
 
 export const updateTodoName = (id, newTaskName) => {
-    Axios.put("http://localhost:3001/update-name", { id: id, todoName: newTaskName })
+    Axios.put("https://crudmern-todo.herokuapp.com/update-name", { id: id, todoName: newTaskName })
 }
 
 export const updateTodo = (id, newTaskName, newDays) => {
-    return Axios.put("http://localhost:3001/update", { id: id, todoName: newTaskName, days: newDays })
+    return Axios.put("https://crudmern-todo.herokuapp.com/update", { id: id, todoName: newTaskName, days: newDays })
 }
 
 export const deleteTodo = (id) => {
-    Axios.delete(`http://localhost:3001/delete/${id}`)
+    Axios.delete(`https://crudmern-todo.herokuapp.com/delete/${id}`)
 }
 
 // testing
 export const updateTodoAndFetch = (id, newTaskName, newDays) => {
-    return Axios.post("http://localhost:3001/update-and-fetch", { id: id, todoName: newTaskName, days: newDays })
+    return Axios.post("https://crudmern-todo.herokuapp.com/update-and-fetch", { id: id, todoName: newTaskName, days: newDays })
 }
